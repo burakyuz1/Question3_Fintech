48245503acde157f39a9952d01084532463b1d1b
Burak <burakyuzz@gmail.com>
2024-09-19 22:40:29 +0300

    Add project files.

---
+++ Models/CalenderResponseModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Question3_Fintech.Models
+{
+    public record CalenderResponseModel(int Year, MonthRecord[] Months);
+
+    public record MonthRecord(byte MonthIndex, DayRecord[] Days, bool IsSelectable);
+
+    public record DayRecord(
+        byte DayTitle,
+        DayOfWeek DayOfWeek,
+        int PositionCount,
+        ProfitLossData ProfitLossData
+    );
+
+    /// <summary>
+    /// model for selection of Profit in USDT or %, it should render according to selection of user
+    /// </summary>
+    /// <param name="Usdt"></param>
+    /// <param name="Percentage"></param>
+    public record ProfitLossData(
+        decimal Usdt,
+        decimal Percentage
+    );
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, Czechia!");
+
+//*
+// Hello,
+
+// Here i assumed we get the all year-months pair from api call, i dont know the project size. if this data getting from complex way we should pass 2 parameters
+// (monthIndex, year) -> and we can get just days. so when user click each year or month, we send a request and get days and render into calender.
+
+
+// as i mention above i assume we got ALL year/months pair as a response. So FE developer render the months (selectable or not) and ALL years. (top side)
+// we will going to return CalenderResponseModel[] as an api result.
+// FE dev. will going to render years and months. When user click specific month/year, developer going to render related dayData[] and render it into his/her calender.
+
+
+// please check the dummy.json because in my case he/she will get the array of CalenderResponseModel[] and i just put the oct/2019 until 20th of month.
+
+
+// please feedback, thank you in advance.
+
+// :) 
+//*/
